f4b534981ddc8ca230e2156778c8ef338bbbf740
Yassir hakimi <yassirhakimi60@gmail.com>
2024-07-16 17:45:55 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { Stack, Link } from 'expo-router';
+
+import { Button } from '~/components/Button';
+import { Container } from '~/components/Container';
+import { ScreenContent } from '~/components/ScreenContent';
+
+require('dotenv').config();
+
+
+export default function Home() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Home' }} />
+      <Container>
+        <ScreenContent path="app/index.tsx" title="Home" />
+        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
+          <Button title="Show Details" />
+        </Link>
+      </Container>
+    </>
+  );
+}
